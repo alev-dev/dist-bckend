4bd16f957be6ff185f295f8973a43681c701fe39
alejandro.alfonso1 <alejandro.alfonso@onlive.site>
2022-11-27 18:40:27 -0300

    order timeStamp

--- src/models/order.js
+++ src/models/order.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const orderSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    products: [
-        {
-            product: {
-                type: Schema.Types.ObjectId,
-                ref: 'Product',
-            },
-            quantity: Number,
+const orderSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
         },
-    ],
-    paymentForm: String,
-    total: Number,
-});
+        products: [
+            {
+                product: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Product',
+                },
+                quantity: Number,
+            },
+        ],
+        paymentForm: String,
+        total: Number,
+    },
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = model('Order', orderSchema);
