f704b92eccdd14a6129d43df3611bd52d2c3ed69
alejandro.alfonso1 <alejandro.alfonso@onlive.site>
2022-11-27 17:16:56 -0300

    statistic functions

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -79,6 +79,69 @@ const orderController = {
                 res.json(err);
             });
     },
+
+    getProductsMoreSoldWithImage: async (req, res) => {
+        await orderModel
+            .aggregate([
+                {
+                    $unwind: '$products',
+                },
+                {
+                    $group: {
+                        _id: '$products.product',
+                        total: { $sum: '$products.quantity' },
+                    },
+                },
+                {
+                    $sort: { total: -1 },
+                },
+                {
+                    $limit: 5,
+                },
+                {
+                    $lookup: {
+                        from: 'products',
+                        localField: '_id',
+                        foreignField: '_id',
+                        as: 'product',
+                    },
+                },
+                {
+                    $unwind: '$product',
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        name: '$product.name',
+                        image: '$product.image',
+                        total: 1,
+                    },
+                },
+            ])
+            .then((products) => {
+                res.json(products);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    },
+
+    getOrderLast24Hours: async (req, res) => {
+        await orderModel
+            .find({
+                createdAt: {
+                    $gte: new Date(new Date() - 24 * 60 * 60 * 1000),
+                },
+            })
+            .populate('user')
+            .populate('products.product')
+            .then((orders) => {
+                res.json(orders);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    },
 };
 
 module.exports = orderController;
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,6 +1,15 @@
 const { Router } = require('express');
 const router = Router();
-const { createOrder, getOrder, getOrders, getOrdersByUser, updateOrder, deleteOrder } = require('../controllers/order');
+const {
+    createOrder,
+    getOrder,
+    getOrders,
+    getOrdersByUser,
+    updateOrder,
+    deleteOrder,
+    getOrderLast24Hours,
+    getProductsMoreSoldWithImage,
+} = require('../controllers/order');
 
 router.get('/:id', getOrder);
 router.get('/', getOrders);
@@ -8,5 +17,7 @@ router.get('/user/:id', getOrdersByUser);
 router.post('/', createOrder);
 router.put('/:id', updateOrder);
 router.delete('/:id', deleteOrder);
+router.get('/est/last24hours', getOrderLast24Hours);
+router.get('/est/products/moreSold', getProductsMoreSoldWithImage);
 
 module.exports = router;
